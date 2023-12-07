c95f882a51af44a0effe801af9241ab34d29a285
beanbeansprout <55658099+beanbeansprout@users.noreply.github.com>
2023-12-06 23:39:54 -0500

    fixed removeRecord overwriting to blank file

--- Application.py
+++ Application.py
@@ -2,6 +2,22 @@ from FileManager import FileManager
 
 fileManager = FileManager()
 
+def promptInt(message):
+    while True:
+        try:
+            return int(input(message))
+        except:
+            print('Invalid input. Enter an integer')
+
+def displaySearchResult(foundRecords):
+    if foundRecords == []:
+        print('No record with this ID was found')
+    else:
+        print('Found Records:')
+
+        for i in range(len(foundRecords)):
+            print('ID: ' + foundRecords[i][0] + ', Name: ' + foundRecords[i][1] + ', Artist: ' + foundRecords[i][2] + ', Label: ' + foundRecords[i][3])
+
 def showMainMenu():
     userInput = input('Select operation - Add record (1), Search records (2), Edit record (3), Remove record (4), Exit (5): ')
 
@@ -19,13 +35,7 @@ def showMainMenu():
         print('Invalid input')
 
 def showAddMenu():
-    while True:
-        try:
-            id = int(input('Enter ID: '))
-            break
-        except:
-            print('Invalid input. Enter an integer')
-
+    id = promptInt('Enter ID: ')
     name = input('Enter Name: ')
     artist = input('Enter Artist: ')
     label = input('Enter Label: ')
@@ -35,19 +45,32 @@ def showAddMenu():
     print('Record successfully added')
 
 def showSearchMenu():
-    return
+    userInput = input('Search for records by - ID (1), Name (2), Artist (3), Label (4), Exit (5): ')
+
+    if userInput == '1':
+        id = promptInt('Enter ID: ')
+        foundRecord = fileManager.getRecordById(id)
+
+        print('ID: ' + foundRecord[0] + ', Name: ' + foundRecord[1] + ', Artist: ' + foundRecord[2] + ', Label: ' + foundRecord[3])
+    elif userInput == '2':
+        foundRecords = fileManager.searchRecordsByName(input('Enter name: '))
+        displaySearchResult(foundRecords)
+    elif userInput == '3':
+        foundRecords = fileManager.searchRecordsByArtist(input('Enter artist: '))
+        displaySearchResult(foundRecords)
+    elif userInput == '4':
+        foundRecords = fileManager.searchRecordsByLabel(input('Enter label: '))
+        displaySearchResult(foundRecords)
+    elif userInput == '5':
+        return
+    else:
+        print('Invalid input')
 
 def showEditMenu():
     return
 
 def showRemoveMenu():
-    while True:
-        try:
-            id = int(input('Enter ID of record to remove: '))
-            break
-        except:
-            print('Invalid input. Enter an integer')
-
+    id = promptInt('Enter ID of record to remove: ')
     fileManager.removeRecord(id)
 
 while True:
--- FileManager.py
+++ FileManager.py
@@ -1,8 +1,10 @@
 import csv
+import os
 
 class FileManager:
     def __init__(self):
         self._FILE_PATH = 'data.csv'
+        self._TEMP_FILE_PATH = 'data_temp.csv'
 
     def getNumRecords(self):
         numRecords = 0
@@ -14,6 +16,11 @@ class FileManager:
                 numRecords += 1
 
         return numRecords
+    
+    def addRecord(self, id, name, artist, label):
+        with open(self._FILE_PATH, 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([id, name, artist, label])
 
     def getRecordById(self, id):
         with open(self._FILE_PATH, 'r') as file:
@@ -24,25 +31,56 @@ class FileManager:
                     return row
                 
         return False
-    
-    def addRecord(self, id, name, artist, label):
-        with open(self._FILE_PATH, 'a', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow([id, name, artist, label])
 
+    def searchRecordsByName(self, name):
+        foundRecords = []
+
+        with open(self._FILE_PATH, 'r') as file:
+            reader = csv.reader(file)
+
+            for row in reader:
+                if row[1] == name:
+                    foundRecords.append(row)
+
+        return foundRecords
+    
+    def searchRecordsByArtist(self, artist):
+        foundRecords = []
+
+        with open(self._FILE_PATH, 'r') as file:
+            reader = csv.reader(file)
+
+            for row in reader:
+                if row[2] == artist:
+                    foundRecords.append(row)
+
+        return foundRecords
+    
+    def searchRecordsByLabel(self, label):
+        foundRecords = []
+
+        with open(self._FILE_PATH, 'r') as file:
+            reader = csv.reader(file)
+
+            for row in reader:
+                if row[3] == label:
+                    foundRecords.append(row)
+
+        return foundRecords
+    
+    def editRecord(self, id, fieldIndex):
+        return
+    
     def removeRecord(self, id):
         record = self.getRecordById(id)
 
-        with open(self._FILE_PATH, 'r+', newline='') as file:
-            reader = csv.reader(file)
-            writer = csv.writer(file)
-            
+        with open(self._FILE_PATH, 'r') as readFile, open(self._TEMP_FILE_PATH, 'w', newline='') as writeFile:
+            reader = csv.reader(readFile)
+            writer = csv.writer(writeFile)
+
             for row in reader:
                 if row[0] != record[0]:
                     writer.writerow(row)
-    
-    def searchRecordsByArtist(self):
-        return
-    
-    def searchRecordsByLabel(self):
-        return
+
+        os.remove(self._FILE_PATH)
+        os.rename(self._TEMP_FILE_PATH, self._FILE_PATH)
