0264c8e426e9ca371978bcb251c7614db727fc76
beanbeansprout <55658099+beanbeansprout@users.noreply.github.com>
2023-12-07 19:32:08 -0500

    comments

--- Application.py
+++ Application.py
@@ -2,36 +2,36 @@ from FileManager import FileManager
 
 fileManager = FileManager()
 
-def promptInt(message):
+def promptInt(message): # continuously prompts the user for an integer input
     while True:
         try:
             return int(input(message))
         except:
             print('Invalid input. Enter an integer')
 
-def promptSelection(message, min, max):
+def promptSelection(message, min, max): # continuously prompts the user for a number that corresponds to an operation, within a certain range
     while True:
-        try: 
+        try: # checks if input is integer
             selection = int(input(message))
         except:
             print('Invalid input. Enter an integer')
             continue
 
-        if selection >= min and selection <= max:
+        if selection >= min and selection <= max: # checks if input is within range
             return selection
         else:
             print('Invalid input. Operation not in range')
 
-def displaySearchResult(foundRecords):
-    if foundRecords != []:
+def displaySearchResult(foundRecords): # prints a search result from a list of returned csv rows (also a list)
+    if foundRecords != []: # an empty list means no rows were added to the list
         print('Found Record(s):')
 
-        for i in range(len(foundRecords)):
+        for i in range(len(foundRecords)): # prints the csv row data
             print('ID: ' + foundRecords[i][0] + ', Name: ' + foundRecords[i][1] + ', Artist: ' + foundRecords[i][2] + ', Label: ' + foundRecords[i][3])
     else:
         print('No records found')
 
-def showMainMenu():
+def showMainMenu(): # shows the main menu
     operation = promptSelection('Select operation - Add record (1), Search records (2), Edit record (3), Remove record (4), Exit (5): ', 1, 5)
 
     if operation == 1:
@@ -45,39 +45,39 @@ def showMainMenu():
     elif operation == 5:
         quit()
 
-def showAddMenu():
+def showAddMenu(): # shows the add record menu
     id = promptInt('Enter ID: ')
     name = input('Enter Name: ')
     artist = input('Enter Artist: ')
     label = input('Enter Label: ')
 
-    if fileManager.addRecord(id, name, artist, label):
+    if fileManager.addRecord(id, name, artist, label): # addRecord returns true if successful, false if unsuccessful
         print('Record successfully added')
     else:
         print('Addition failed. Record with desired ID already exists, or an invalid ID was chosen (0 or less)')
 
-def showSearchMenu():
+def showSearchMenu(): # shows the search records menu
     searchMethod = promptSelection('Search for records by - ID (1), Name (2), Artist (3), Label (4): ', 1, 4)
-    fieldValue = input('Enter field value: ')
+    fieldValue = input('Enter field value: ') # the new desired value for a certain field, specified by a selection prompt
 
-    foundRecords = fileManager.searchRecords(searchMethod, fieldValue)
+    foundRecords = fileManager.searchRecords(searchMethod, fieldValue) # get search result
     displaySearchResult(foundRecords)
 
-def showEditMenu():
+def showEditMenu(): # shows the edit record menu
     id = promptInt('Enter ID of record to edit: ')
     fieldId = promptSelection('Enter field to edit - ID (1), Name (2), Artist (3), Label (4): ', 1, 4)
-    newValue = input('Enter new field value: ')
+    newValue = input('Enter new field value: ') # the new desired value for a certain field, specified by a selection prompt
 
-    if fileManager.editRecord(id, fieldId, newValue) == False:
+    if fileManager.editRecord(id, fieldId, newValue) == False: # returns false if edit was unsuccessful, which only happens when a nonexistent ID was chosen
         print('Edit failed. Record ID does not exist')
 
-def showRemoveMenu():
+def showRemoveMenu(): # shows the remove record menu
     id = promptInt('Enter ID of record to remove: ')
 
-    if fileManager.removeRecord(id):
+    if fileManager.removeRecord(id): # returns true if successful, false if unsuccessful (which only happens when a nonexistent ID was chosen)
         print('Successfully removed record')
     else:
         print('Record ID does not exist')
 
-while True:
+while True: # main loop
     showMainMenu()
--- FileManager.py
+++ FileManager.py
@@ -1,106 +1,105 @@
 import csv
 import os
 
-class FileManager:
-    def __init__(self):
+class FileManager: # handles reading and writing to the csv file
+    def __init__(self): # stores file path constants
         self._FILE_PATH = 'data.csv'
         self._TEMP_FILE_PATH = 'data_temp.csv'
 
-    def getNumRecords(self):
+    def getNumRecords(self): # get the number of rows from the file
         numRecords = 0
 
         with open(self._FILE_PATH, 'r') as file:
             reader = csv.reader(file)
 
+            # count numbe of rows
             for row in reader:
                 numRecords += 1
 
         return numRecords
     
-    def getRecordById(self, id):
+    def getRecordById(self, id): # get a record (a row in the file) from its ID.
         with open(self._FILE_PATH, 'r') as file:
             reader = csv.reader(file)
 
             for row in reader:
-                if row[0] == str(id):
-                    return row
+                if row[0] == str(id): # checks if the first attribute of the row is equal to the specified ID
+                    return row # returns the whole row as a list
                 
-        return False
+        return False # returns false if no record with specified ID was found
     
-    def addRecord(self, id, name, artist, label):
-        if id <= 0:
+    def addRecord(self, id, name, artist, label): # add a row to the file
+        if id <= 0: # record IDs should be 1 or greater
             return False
 
         with open(self._FILE_PATH, 'r') as file:
             reader = csv.reader(file)
 
             for row in reader:
-                if row[0] == str(id):
-                    return False
+                if row[0] == str(id): # checks if the specified ID already belongs to a record
+                    return False # ensures that each record has a unique ID
 
         with open(self._FILE_PATH, 'a', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow([id, name, artist, label])
+            writer.writerow([id, name, artist, label]) # append record data to file
 
         return True
                     
-    def searchRecords(self, searchMethod, value):
-        foundRecords = []
+    def searchRecords(self, searchMethod, value): # search for rows in the file, given a certain field attribute as a key (either ID, name, artist, or label)
+        foundRecords = [] # for storing the rows that resulted from the search
 
         with open(self._FILE_PATH, 'r') as file:
             reader = csv.reader(file)
 
             for row in reader:
-                if row[searchMethod - 1] == value:
+                if row[searchMethod - 1] == value: # searchMethod contains an int from 1 to 4, row attribute indexes are from 0 to 3
                     foundRecords.append(row)
                 
         return foundRecords
     
-    def editRecord(self, id, fieldIndex, newValue):
-        record = self.getRecordById(id)
+    def editRecord(self, id, fieldIndex, newValue): # edit a row in the file from its ID
+        record = self.getRecordById(id) # get a row from the file from its ID
 
-        if record == False:
+        if record == False: # makes sure a row was actually chosen
             return False
         
-        with open(self._FILE_PATH, 'r') as readFile, open(self._TEMP_FILE_PATH, 'w', newline='') as writeFile:
+        with open(self._FILE_PATH, 'r') as readFile, open(self._TEMP_FILE_PATH, 'w', newline='') as writeFile: # read current file and write to a temporary file
             reader = csv.reader(readFile)
             writer = csv.writer(writeFile)
 
             for row in reader:
-                if row[0] == record[0]:
-                    if fieldIndex == 1:
+                if row[0] == record[0]: # if the record ID in the row matches with the specified ID
+                    if fieldIndex == 1: # change ID
                         writer.writerow([newValue, row[1], row[2], row[3]])
-                    elif fieldIndex == 2:
+                    elif fieldIndex == 2: # change name
                         writer.writerow([row[0], newValue, row[2], row[3]])
-                    elif fieldIndex == 3:
+                    elif fieldIndex == 3: # change artist
                         writer.writerow([row[0], row[1], newValue, row[3]])
-                    elif fieldIndex == 4:
+                    elif fieldIndex == 4: # change label
                         writer.writerow([row[0], row[1], row[2], newValue])
-                    else:
-                        return False
                 else:
-                    writer.writerow(row)
+                    writer.writerow(row) # copy the rows from the read file to the temporary file
 
-        os.remove(self._FILE_PATH)
-        os.rename(self._TEMP_FILE_PATH, self._FILE_PATH)
+        os.remove(self._FILE_PATH) # delete the file being read from
+        os.rename(self._TEMP_FILE_PATH, self._FILE_PATH) # rename the temporary file to the current file to replace the file being read from
 
         return True
     
-    def removeRecord(self, id):
-        record = self.getRecordById(id)
+    def removeRecord(self, id): # removes a row from the file from its ID
+        record = self.getRecordById(id) # get a row from the file from its ID
 
-        if record == False:
+        if record == False: # make sure a row was actually chosen
             return False
 
-        with open(self._FILE_PATH, 'r') as readFile, open(self._TEMP_FILE_PATH, 'w', newline='') as writeFile:
+        with open(self._FILE_PATH, 'r') as readFile, open(self._TEMP_FILE_PATH, 'w', newline='') as writeFile: # read current file and write to a temporary file
             reader = csv.reader(readFile)
             writer = csv.writer(writeFile)
 
             for row in reader:
-                if row[0] != record[0]:
+                if row[0] != record[0]: # copies every row from the read file to the temporary file, excluding the row with the specified ID
                     writer.writerow(row)
 
-        os.remove(self._FILE_PATH)
-        os.rename(self._TEMP_FILE_PATH, self._FILE_PATH)
+        os.remove(self._FILE_PATH) # delete the file being read from
+        os.rename(self._TEMP_FILE_PATH, self._FILE_PATH) # rename the temporary file to the current file to reaplce the file being read from
 
         return True
